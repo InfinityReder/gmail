c77dca7fd2a80d3e7da9a1d21985455359dd3d65
IRR <irr@IRRdeMacBook-Pro.local>
2017-06-09 19:27:26 +0800

    add

--- db_ideas.py
+++ db_ideas.py
@@ -19,9 +19,37 @@ class Ideas(BaseModel):
     direction = IntegerField()
     score = IntegerField()
 
-# def saveIdea(user_name = '',idea_id = '',title = '',contents = '',contents = '',comments = '',direction = None,score = None):
-#     return Ideas.create(user_name = user_name,idea_id = idea_id,title = title,contents = contents, comments = comments, direction = direction,score = score)
+def saveIdea(user_name = '',idea_id = '',title = '',contents = '',comments = '',direction = None,score = None):
+    if(not idea_id):
+        return False
+    return Ideas.create(user_name = user_name,idea_id = idea_id,title = title,contents = contents, comments = comments, direction = direction,score = score)
+
+def saveIdeaWithCheck( user_name = '',idea_id = '',title = '',contents = '',comments = '',direction = None,score = None ):
+    if(not idea_id):
+        return False
+    sqlIns = Ideas.select(Ideas,fn.Count(Ideas.id).alias('count')).where(Ideas.idea_id == idea_id)
+    count = sqlIns.get().count
+    if(count > 0):
+        for item in sqlIns:
+            updateIdeas(user_name = user_name,idea_id = idea_id,title = title,contents = contents, comments = comments, direction = direction,score = score,executeIns = item)
+    else:
+        saveIdea(user_name = user_name,idea_id = idea_id,title = title,contents = contents, comments = comments, direction = direction,score = score)
+
+def updateIdeas(user_name = '',idea_id = '',title = '',contents = '',comments = '',direction = None,score = None ,executeIns = None):
+    if(executeIns):
+        executeIns.contents = contents
+        executeIns.comments = comments
+        executeIns.direction = direction
+        executeIns.score = score
+        executeIns.save()
+    else:
+        if(not idea_id):
+            return False
+        Ideas.update(user_name = user_name,title = title,contents = contents, comments = comments, direction = direction,score = score).where(idea_id == idea_id).execute()
 
 def getIdeas(pageNum = 1,pageSize = 20):
-    return Ideas.select().paginate(pageNum,paginate_by = pageSize).get()
+    return Ideas.select().paginate(pageNum,paginate_by = pageSize).execute()
+    
+if __name__ == '__main__':
+    saveIdeaWithCheck(idea_id = '?',contents = 'hhhhhh')
     
